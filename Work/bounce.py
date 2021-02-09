2e502f524b2c857d06b9177b8ee9bb8e96004176
coyoteran <55897124+coyoteran@users.noreply.github.com>
2021-02-09 13:57:36 -0600

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100 #first drop in meters
+bounces = 1 
+rebound = 3/5
+
+while bounces < 11;
+    print(bounces, height * rebound)
+    bounces = bounces + 1
+    height = height * rebound
+print('Number of bounces', bounces)
+print('Height', height)
+  
